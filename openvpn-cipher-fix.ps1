377d8d464586b15042ed8c2748897d9419316543
Jason Johnson <jason.johnson@integrisit.com>
2023-08-16 15:35:51 -0500

    Added some verbose output

--- openvpn-cipher-fix.ps1
+++ openvpn-cipher-fix.ps1
@@ -19,17 +19,21 @@
   <Example goes here. Repeat this attribute for more than one example>
 #>
 # Function to add line after match
-
 function Add-LineAfterMatch {
-  $path = $args[0]  # Get the first argument passed to the function as the path
-  $match = 'cipher'
-  $insertText = 'data-ciphers AES-256-CBC'
+  $path = $args[0]
+  $match = "cipher"
+  $insertText = "data-ciphers AES-256-CBC"
+
+  if (-not (Test-Path $path)) {
+      Write-Host "File $path does not exist." -ForegroundColor Red
+      return
+  }
 
   $fileContent = Get-Content -Path $path
-  $alreadyExists = $fileContent -like "*$insertText*"
+  $alreadyExists = $fileContent -contains $insertText
 
-  # Check if the insertText already exists
   if ($alreadyExists) {
+      Write-Host "File $path already contains the line '$insertText'. Skipping..." -ForegroundColor Yellow
       return
   }
 
@@ -40,27 +44,32 @@ function Add-LineAfterMatch {
       $output += $line
       if ($line -like "$match*") {
           $matchFound = $true
+          Write-Host "Match found in file $path." -ForegroundColor Cyan
+      }
+
+      if ($matchFound -and ($line -notlike "$insertText")) {
           $output += $insertText
+          $matchFound = $false
+          Write-Host "Inserted line '$insertText' in file $path." -ForegroundColor Green
       }
   }
 
-  # Save only if a match was found and we made modifications
-  if ($matchFound) {
-      $output | Set-Content -Path $path
-  }
+  $output | Set-Content -Path $path
 }
 
-# Paths to search
+# Define paths
 $defaultUserPath = "C:\Users\*\OpenVPN\config"
 $programPath = "C:\Program Files\OpenVPN\config"
 
-# Get all .ovpn files in the paths and their subdirectories
+# Get a list of all .ovpn files in the defined paths and subfolders
 $files = Get-ChildItem -Path $defaultUserPath, $programPath -Recurse -Filter "*.ovpn"
 
+Write-Host "Searching for .ovpn files to process..." -ForegroundColor Cyan
+
 foreach ($file in $files) {
-  Add-LineAfterMatch $file.FullName  # Here we're just passing the file path as an argument
+  Write-Host "Processing file: $file.FullName" -ForegroundColor Magenta
+  Add-LineAfterMatch $file.FullName
 }
 
-Write-Output "Operation completed successfully!"
-
-
+Write-Host "Processing complete. Press any key to exit..." -ForegroundColor Magenta
+$host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown") | Out-Null
